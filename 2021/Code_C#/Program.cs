db2c49506c39528081a8645af8dfb2d5819b4907
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-27 17:49:49 +0500

    Add Code to Program file

--- 2021/Code_C#/Program.cs
+++ 2021/Code_C#/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] GetArray()
+{
+    Random r = new Random();
+    int[] Array = new int[r.Next(10, 20)];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = r.Next(-99, 100);
+    }
+    return Array;
+}
+
+(int[], int) EvenIntegers(int[] Array)
+{
+    int[] mass = new int[Array.Length];
+    int j = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] % 2 == 0)
+        {
+            mass[j] = Array[i];
+            j++;
+        }
+    }
+    return (mass, j);
+}
+
+void PrintArray(int[] Array, int arg)
+{
+    if (arg == 0) arg = Array.Length;
+    for (int i=0; i<arg;i++)
+        {
+            Console.Write($"{Array[i]} ");
+        }
+    Console.WriteLine();       
+}
+
+
+int[] Array = GetArray();
+(int[] Even, int evenNum) = EvenIntegers(Array);
+
+Console.Clear();
+PrintArray(Array, 0);
+PrintArray(Even, evenNum);
