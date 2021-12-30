1c6118502b195238b7895c12ccfd00dd442603a7
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2021-12-30 20:39:28 +0500

    Add comments and Array.Resize variant

--- 2021/Code_C#/Program.cs
+++ 2021/Code_C#/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] GetArray()
+int[] GetArray()// метод создания и заполнения массива
 {
     Random r = new Random();
     int[] Array = new int[r.Next(10, 20)];
@@ -9,7 +9,7 @@
     return Array;
 }
 
-(int[], int) GetEvenIntegers(int[] Array)
+(int[], int) GetEvenIntegers(int[] Array)//метод нахождения четных чисел и создания нового массива из них
 {
     int[] mass = new int[Array.Length];
     int j = 0;
@@ -21,10 +21,11 @@
             j++;
         }
     }
+    //Array.Resize(ref mass, j); возможен вариант изменения размера массива, в соответствии с количеством значимых элементов в нем;
     return (mass, j);
 }
 
-void PrintArray(int[] Array, int arg)
+void PrintArray(int[] Array, int arg)//метод печати массивов в консоль
 {
     if (arg == 0) arg = Array.Length;
     for (int i=0; i<arg;i++)
@@ -34,7 +35,7 @@ void PrintArray(int[] Array, int arg)
     Console.WriteLine();       
 }
 
-
+// Т Е Л О    П Р О Г Р А М М Ы
 int[] Array = GetArray();
 (int[] Even, int evenNum) = GetEvenIntegers(Array);
 
